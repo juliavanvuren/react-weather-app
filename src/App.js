b6002a5171622d80c89442a5e5ab3944534aaa3f
Julia <107463763+juliavanvuren@users.noreply.github.com>
2022-09-20 11:27:32 +0200

    Removed unnessesary files, installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
+    
       <h1>
        React Weather App
       </h1>
+<span>Open-source code by Julia van Vuren</span>
+    </div>
     </div>
   );
 }
